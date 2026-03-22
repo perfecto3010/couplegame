24bb607cf04c5852b31b9b974a1334123f9fc08e
perfecto3010 <perfecto3010@gmail.com>
2026-03-22 23:16:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
-const cacheName = 'lahalibo-v1';
+const cacheName = 'Gahez-v2.3';
 const assets = [
   './',
   './index.html',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap'
+  './gamesData.js',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/833/833472.png'
 ];
 
-// Install Service Worker
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching assets');
-      cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Fetching assets
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
